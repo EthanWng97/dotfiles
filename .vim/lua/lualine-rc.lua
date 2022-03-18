21471bfd835aa6484a0af1ebaadc4fe73b644063
Yifan Wang <yif.wang@hotmail.com>
2022-03-18 14:30:52 -0700

    feat: enable global statusline

--- .vim/lua/lualine-rc.lua
+++ .vim/lua/lualine-rc.lua
@@ -1,42 +1,40 @@
 local function diff_source()
-  local gitsigns = vim.b.gitsigns_status_dict
-  if gitsigns then
-    return {
-      added = gitsigns.added,
-      modified = gitsigns.changed,
-      removed = gitsigns.removed
-    }
-  end
+    local gitsigns = vim.b.gitsigns_status_dict
+    if gitsigns then
+        return {
+            added = gitsigns.added,
+            modified = gitsigns.changed,
+            removed = gitsigns.removed
+        }
+    end
 end
 
 require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'vscode',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {},
-    always_divide_middle = true,
-  },
-  sections = {
-    lualine_a = {'branch'},
-    lualine_b = {{'diff', source = diff_source}, 'diagnostics'},
-    lualine_c = {'lsp_progress', 'g:coc_status'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  extensions = {
-	'fugitive',
-	'fzf',
-  }
+    options = {
+        icons_enabled = true,
+        theme = 'vscode',
+        component_separators = {left = '', right = ''},
+        section_separators = {left = '', right = ''},
+        disabled_filetypes = {},
+        always_divide_middle = true,
+        globalstatus = true
+    },
+    sections = {
+        lualine_a = {'branch'},
+        lualine_b = {{'diff', source = diff_source}, 'diagnostics'},
+        lualine_c = {'lsp_progress', 'g:coc_status', 'filename'},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+    },
+    tabline = {},
+    extensions = {'fugitive', 'fzf'}
 }
