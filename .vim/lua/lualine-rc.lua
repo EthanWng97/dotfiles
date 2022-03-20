b81e8da7a08830a830020d442d4f609e5b3903ec
Yifan Wang <yif.wang@hotmail.com>
2022-03-19 20:27:52 -0700

    chore: set theme to auto

--- .vim/lua/lualine-rc.lua
+++ .vim/lua/lualine-rc.lua
@@ -12,7 +12,7 @@ end
 require('lualine').setup {
     options = {
         icons_enabled = true,
-        theme = 'vscode',
+        theme = 'auto',
         component_separators = {left = '', right = ''},
         section_separators = {left = '', right = ''},
         disabled_filetypes = {},
