a4dd55de67fe0ea9267d9a1632d926a566a2cebf
Yifan Wang <yif.wang@hotmail.com>
2022-03-19 21:23:20 -0700

    feat: everything is Dracula

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -41,3 +41,4 @@ set fzf_preview_dir_cmd exa --all --color=always --icons
 set fzf_fd_opts --hidden
 starship init fish --print-full-init | source
 set -gx EDITOR nvim
+set -Ux FZF_DEFAULT_OPTS --cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*" --color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4
