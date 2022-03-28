1f496eab3d647949a5d86556ebde214c5035c6db
Yifan Wang <yif.wang@hotmail.com>
2022-03-28 13:41:39 -0700

    feat: border style

--- .vim/lua/nvim-lspconfig-rc.lua
+++ .vim/lua/nvim-lspconfig-rc.lua
@@ -2,9 +2,9 @@
 vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
 vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
 local border = {
-    {"┌", "FloatBorder"}, {"─", "FloatBorder"}, {"┐", "FloatBorder"},
-    {"│", "FloatBorder"}, {"┘", "FloatBorder"}, {"─", "FloatBorder"},
-    {"└", "FloatBorder"}, {"│", "FloatBorder"}
+    {"╭", "FloatBorder"}, {"─", "FloatBorder"}, {"╮", "FloatBorder"},
+    {"│", "FloatBorder"}, {"╯", "FloatBorder"}, {"─", "FloatBorder"},
+    {"╰", "FloatBorder"}, {"│", "FloatBorder"}
 }
 
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
