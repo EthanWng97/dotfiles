b34b9225d4bba916db4a60b2457f4f05f518c55c
Yifan Wang <yif.wang@hotmail.com>
2022-04-03 03:43:22 -0700

    feat: notify config

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -128,3 +128,11 @@ require"lspconfig".efm.setup {
     },
     filetypes = {'lua', 'css', 'yaml', 'markdown'}
 }
+vim.notify = function(msg, log_level, _opts)
+    if msg:match("exit code") then return end
+    if log_level == vim.log.levels.ERROR then
+        vim.api.nvim_err_writeln(msg)
+    else
+        vim.api.nvim_echo({{msg}}, true, {})
+    end
+end
