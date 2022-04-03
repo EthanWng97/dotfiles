21d03e0b34331aaaededd5e59a80bf659e44c1d8
Yifan Wang <yif.wang@hotmail.com>
2022-04-03 00:52:00 -0700

    feat: gitsigns config

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -8,38 +8,61 @@ require('gitsigns').setup {
     },
     signs = {
         add = {
-            hl = 'DiffAdd',
-            text = '┃',
-            numhl = 'GitSignsAddNr',
-            linehl = 'GitSignsAddLn'
+            hl = "GitSignsAdd",
+            text = "▎",
+            numhl = "GitSignsAddNr",
+            linehl = "GitSignsAddLn"
         },
         change = {
-            hl = 'DiffChange',
-            text = '┃',
-            numhl = 'GitSignsChangeNr',
-            linehl = 'GitSignsChangeLn'
+            hl = "GitSignsChange",
+            text = "▎",
+            numhl = "GitSignsChangeNr",
+            linehl = "GitSignsChangeLn"
         },
-        delette = {
-            hl = 'DiffDelete',
-            text = '▸',
-            numhl = 'GitSignsDeleteNr',
-            linehl = 'GitSignsDeleteLn'
+        delete = {
+            hl = "GitSignsDelete",
+            text = "契",
+            numhl = "GitSignsDeleteNr",
+            linehl = "GitSignsDeleteLn"
         },
         topdelete = {
-            hl = 'DiffDelete',
-            text = '▸',
-            numhl = 'GitSignsDeleteNr',
-            linehl = 'GitSignsDeleteLn'
+            hl = "GitSignsDelete",
+            text = "契",
+            numhl = "GitSignsDeleteNr",
+            linehl = "GitSignsDeleteLn"
         },
         changedelete = {
-            hl = 'DiffChange',
-            text = '~',
-            numhl = 'GitSignsChangeNr',
-            linehl = 'GitSignsChangeLn'
+            hl = "GitSignsChange",
+            text = "▎",
+            numhl = "GitSignsChangeNr",
+            linehl = "GitSignsChangeLn"
         }
     },
     signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
     numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
     linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
-    word_diff = false -- Toggle with `:Gitsigns toggle_word_diff`
+    word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+    watch_gitdir = {interval = 1000, follow_files = true},
+    attach_to_untracked = true,
+    current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = false
+    },
+    current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+    sign_priority = 6,
+    update_debounce = 100,
+    status_formatter = nil, -- Use default
+    max_file_length = 40000,
+    preview_config = {
+        -- Options passed to nvim_open_win
+        border = "single",
+        style = "minimal",
+        relative = "cursor",
+        row = 0,
+        col = 1
+    },
+    yadm = {enable = false}
 }
