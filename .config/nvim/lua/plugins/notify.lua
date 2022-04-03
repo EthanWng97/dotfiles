648b5d919c394a591162bb38645683f3c4cce24b
Yifan Wang <yif.wang@hotmail.com>
2022-04-03 04:13:00 -0700

    feat: longer time for notify

--- .config/nvim/lua/plugins/notify.lua
+++ .config/nvim/lua/plugins/notify.lua
@@ -13,7 +13,7 @@ notify.setup {
     render = "default",
 
     -- Default timeout for notifications
-    timeout = 175,
+    timeout = 3000,
 
     -- For stages that change opacity this is treated as the highlight behind the window
     -- Set this to either a highlight group or an RGB hex value e.g. "#000000"
