2b34bdb5bb3a0cc7ae702324d5c67b9d9d332a05
Yifan Wang <yif.wang@hotmail.com>
2022-04-03 03:52:06 -0700

    feat: lualine config

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -8,14 +8,18 @@ local function diff_source()
         }
     end
 end
+local spaces = function()
+    return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+end
 
+local location = {"location", padding = 0}
 require('lualine').setup {
     options = {
         icons_enabled = true,
         theme = 'auto',
         component_separators = {left = '', right = ''},
         section_separators = {left = '', right = ''},
-        disabled_filetypes = {},
+        disabled_filetypes = {"alpha", "dashboard", "toggleterm"},
         always_divide_middle = true,
         globalstatus = true
     },
@@ -23,9 +27,9 @@ require('lualine').setup {
         lualine_a = {'branch'},
         lualine_b = {{'diff', source = diff_source}, 'diagnostics'},
         lualine_c = {'filename', 'lsp_progress'},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_x = {'encoding', spaces, 'filetype'},
         lualine_y = {'progress'},
-        lualine_z = {'location'}
+        lualine_z = {location}
     },
     inactive_sections = {
         lualine_a = {},
@@ -36,5 +40,5 @@ require('lualine').setup {
         lualine_z = {}
     },
     tabline = {},
-    extensions = {'fugitive', 'fzf'}
+    extensions = {''}
 }
