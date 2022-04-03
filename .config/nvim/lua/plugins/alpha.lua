02734cc23197d1254f13fbf4bc762fa241b1260a
Yifan Wang <yif.wang@hotmail.com>
2022-04-03 02:50:26 -0700

    feat: alpha plugin

---
+++ .config/nvim/lua/plugins/alpha.lua
@@ -0,0 +1,33 @@
+local alpha = require("alpha")
+local dashboard = require("alpha.themes.dashboard")
+
+-- Set header
+dashboard.section.header.val = {
+    "                                                     ",
+    "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+    "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+    "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+    "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+    "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+    "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+    "                                                     "
+}
+
+-- Set menu
+dashboard.section.buttons.val = {
+    dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+    dashboard.button("f", "  > Find file", ":Telescope find_files<CR>"),
+    dashboard.button("g", "  > Find Text   ", ":Telescope live_grep<CR>"),
+    dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+    dashboard.button("l", "  > List session",
+                     ":SessionManager load_session<CR>"),
+    dashboard.button("q", "  > Quit NVIM", ":qa<CR>")
+}
+
+-- Send config to alpha
+alpha.setup(dashboard.opts)
+
+-- Disable folding on alpha buffer
+vim.cmd([[
+    autocmd FileType alpha setlocal nofoldenable
+]])
