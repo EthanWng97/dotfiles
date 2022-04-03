6cce770d9a44e9e56fdcd75063a57ab128b26f25
Yifan Wang <yif.wang@hotmail.com>
2022-04-03 02:50:48 -0700

    feat: ui design

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -16,6 +16,11 @@ require('telescope').setup {
             override_file_sorter = true, -- override the file sorter
             case_mode = "smart_case" -- or "ignore_case" or "respect_case"
             -- the default case_mode is "smart_case"
+        },
+        ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+                -- even more opts
+            }
         }
     }
 }
@@ -25,3 +30,4 @@ require('telescope').load_extension('fzf')
 require"telescope".load_extension("frecency")
 require("telescope").load_extension("file_browser")
 -- require("telescope").load_extension("session-lens")
+require("telescope").load_extension("ui-select")
