d3c395d26ce55d757a3af599a82022321eab1ed5
Yifan Wang <yif.wang@hotmail.com>
2022-04-03 14:40:05 -0700

    feat: dap config

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -1,26 +1,70 @@
 local dap = require('dap')
 local dapui = require('dapui')
--- dap.listeners.after.event_initialized["dapui_config"] =
---     function() dapui.open() end
--- dap.listeners.before.event_terminated["dapui_config"] =
---     function() dapui.close() end
--- dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
 
-dap.adapters.codelldb = function(callback, config)
-    -- specify in your configuration host = your_host , port = your_port
-    callback({type = "server", host = '127.0.0.1', port = 13000})
-end
+dapui.setup {
+    icons = {expanded = "▾", collapsed = "▸"},
+    mappings = {
+        -- Use a table to apply multiple mappings
+        expand = {"<CR>", "<2-LeftMouse>"},
+        open = "o",
+        remove = "d",
+        edit = "e",
+        repl = "r",
+        toggle = "t"
+    },
+    sidebar = {
+        -- You can change the order of elements in the sidebar
+        elements = {
+            -- Provide as ID strings or tables with "id" and "size" keys
+            {
+                id = "scopes",
+                size = 0.25 -- Can be float or integer > 1
+            }, {id = "breakpoints", size = 0.25}
+            -- { id = "stacks", size = 0.25 },
+            -- { id = "watches", size = 00.25 },
+        },
+        size = 40,
+        position = "right" -- Can be "left", "right", "top", "bottom"
+    },
+    tray = {
+        elements = {}
+        -- elements = { "repl" },
+        -- size = 10,
+        -- position = "bottom", -- Can be "left", "right", "top", "bottom"
+    },
+    floating = {
+        max_height = nil, -- These can be integers or a float between 0 and 1.
+        max_width = nil, -- Floats will be treated as percentage of your screen.
+        border = "rounded", -- Border style. Can be "single", "double" or "rounded"
+        mappings = {close = {"q", "<Esc>"}}
+    },
+    windows = {indent = 1}
+}
+
+dap.adapters.cppdbg = {
+    id = 'cppdbg',
+    type = 'executable',
+    command = '/Users/navepnow/Desktop/vscode-cpptools/debugAdapters/bin/OpenDebugAD7'
+}
 
 dap.configurations.cpp = {
     {
         name = "Launch file",
-        type = "codelldb",
+        type = "cppdbg",
         request = "launch",
+        MIMode = 'lldb',
+        miDebuggerPath = '/Users/navepnow/Desktop/vscode-cpptools/debugAdapters/lldb-mi/bin/lldb-mi',
+        cwd = '${workspaceFolder}',
         program = function()
             return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/',
                                 'file')
         end,
-        cwd = '${workspaceFolder}',
         stopOnEntry = true
     }
 }
+
+dap.listeners.after.event_initialized["dapui_config"] =
+    function() dapui.open() end
+dap.listeners.before.event_terminated["dapui_config"] =
+    function() dapui.close() end
+dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
