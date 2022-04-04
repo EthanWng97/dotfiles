9ad16f125323c321afe7853ebf6e6597a9975fce
Yifan Wang <yif.wang@hotmail.com>
2022-04-03 17:43:11 -0700

    chore: hide wave at the end of file

--- .config/nvim/lua/globals.lua
+++ .config/nvim/lua/globals.lua
@@ -14,6 +14,4 @@ vim.opt.autoread = true -- reload files if changed externally
 vim.opt.showcmd = true -- show the command in the status bar
 vim.opt.autoindent = true
 vim.opt.smartindent = true
-vim.wo.fillchars = 'eob: '
-vim.o.sessionoptions =
-    "buffers,curdir,folds,help,tabpages,winsize,resize,winpos,terminal"
+vim.opt.fillchars:append{eob = " "}
