0a3fcdacc26defd66ddd97634a14ce670b1aadc7
Yifan Wang <yif.wang@hotmail.com>
2022-04-10 13:53:54 -0700

    chore: format

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,26 +2,25 @@ require('spellsitter').setup {
     -- Whether enabled, can be a list of filetypes, e.g. {'python', 'lua'}
     enable = true
 }
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
     highlight = {
         enable = true,
         disable = {},
         additional_vim_regex_highlighting = false
     },
-    autopairs = {enable = true},
-    autotag = {enable = true},
-    indent = {enable = true, disable = {}},
-    ensure_installed = "maintained",
+    autopairs = { enable = true },
+    autotag = { enable = true },
+    indent = { enable = true, disable = {} },
+    ensure_installed = "all",
     sync_install = true,
-    ignore_install = {""}, -- List of parsers to ignore installation
+    ignore_install = { "phpdoc" }, -- List of parsers to ignore installation
     refactor = {
         highlight_definitions = {
             enable = true,
             -- Set to false if you have an `updatetime` of ~100.
             clear_on_cursor_move = true
         },
-        highlight_current_scope = {enable = false}
+        highlight_current_scope = { enable = false }
 
     }
 }
-
