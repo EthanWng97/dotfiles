dc156b035719448314dda6ec54ad0dfc77fdf7a3
Yifan Wang <yif.wang@hotmail.com>
2022-04-11 02:54:54 -0700

    fix: conflict between lsp and pattern

--- .config/nvim/lua/plugins/project.lua
+++ .config/nvim/lua/plugins/project.lua
@@ -1,4 +1,5 @@
 require("project_nvim").setup {
-    exclude_dirs = {"~/Downloads"},
+    exclude_dirs = { "~/Downloads" },
+    detection_methods = { "lsp", "pattern" },
     silent_chdir = false
 }
