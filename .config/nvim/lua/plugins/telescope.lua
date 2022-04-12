ac4752f7f7b7f6b5ec02c82922eb6067b6def92b
Yifan Wang <yif.wang@hotmail.com>
2022-04-12 13:18:32 -0700

    chore: format

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,12 +1,12 @@
 local actions = require("telescope.actions")
 require('telescope').setup {
     pickers = {
-        find_files = {hidden = true},
-        live_grep = {additional_args = function(opts) return {"--hidden"} end}
+        find_files = { hidden = true },
+        live_grep = { additional_args = function(opts) return { "--hidden" } end }
     },
     defaults = {
-        mappings = {i = {["<esc>"] = actions.close}},
-        file_ignore_patterns = {"node_modules", ".git"}
+        mappings = { i = { ["<esc>"] = actions.close } },
+        file_ignore_patterns = { "node_modules", ".git" }
     },
 
     extensions = {
@@ -26,7 +26,7 @@ require('telescope').setup {
 }
 require("telescope").load_extension("vimspector")
 require('telescope').load_extension('fzf')
-require"telescope".load_extension("frecency")
+require "telescope".load_extension("frecency")
 require("telescope").load_extension("file_browser")
 -- require("telescope").load_extension("session-lens")
 -- require("telescope").load_extension("ui-select")
