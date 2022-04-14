7cc9056d93e39cf34ea9f31da4613243ba2d6bb5
Yifan Wang <yif.wang@hotmail.com>
2022-04-13 18:02:22 -0700

    chore: ignore clangd for project.nvim

--- .config/nvim/lua/plugins/project.lua
+++ .config/nvim/lua/plugins/project.lua
@@ -1,5 +1,6 @@
 require("project_nvim").setup {
     exclude_dirs = { "~/Downloads", "~/Desktop" },
     detection_methods = { "pattern", "lsp" },
+    ignore_lsp = { "clangd" },
     silent_chdir = false
 }
