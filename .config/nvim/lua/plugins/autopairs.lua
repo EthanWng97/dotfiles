f8539948862b8f1c9724154527cb5836970f5a3c
Yifan Wang <yif.wang@hotmail.com>
2022-05-15 15:09:24 -0700

    chore: format lua

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,10 +1,14 @@
-local npairs = require("nvim-autopairs")
-npairs.setup({check_ts = true}, {disable_filetype = {"TelescopePrompt"}})
-npairs.remove_rule('`')
+local status_ok, npairs = pcall(require, "nvim-autopairs")
+if not status_ok then
+    return
+end
+npairs.setup({ check_ts = true }, { disable_filetype = { "TelescopePrompt" } })
 
 --- setup for cmp
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+    return
+end
 cmp.event:on('confirm_done',
-             cmp_autopairs.on_confirm_done({map_char = {tex = ''}}))
-
+    cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
