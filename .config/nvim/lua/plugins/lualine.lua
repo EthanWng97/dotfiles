dcab51a9a59bcae403f772967bfd52a6e54be21f
Yifan Wang <yif.wang@hotmail.com>
2022-05-20 13:23:12 -0700

    chore: remove unused space conpomnet

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,47 +1,43 @@
 require("nvim-gps").setup()
 local gps = require("nvim-gps")
 local function diff_source()
-    local gitsigns = vim.b.gitsigns_status_dict
-    if gitsigns then
-        return {
-            added = gitsigns.added,
-            modified = gitsigns.changed,
-            removed = gitsigns.removed
-        }
-    end
-end
-
-local spaces = function()
-    return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+	local gitsigns = vim.b.gitsigns_status_dict
+	if gitsigns then
+		return {
+			added = gitsigns.added,
+			modified = gitsigns.changed,
+			removed = gitsigns.removed,
+		}
+	end
 end
 
 local location = { "location", padding = 0 }
-require('lualine').setup {
-    options = {
-        icons_enabled = true,
-        theme = 'auto',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-        disabled_filetypes = { "alpha", "dashboard", "toggleterm" },
-        always_divide_middle = true,
-        globalstatus = true
-    },
-    sections = {
-        lualine_a = { 'branch' },
-        lualine_b = { { 'diff', source = diff_source }, 'diagnostics' },
-        lualine_c = { 'filename', 'lsp_progress', { gps.get_location, cond = gps.is_available } },
-        lualine_x = { 'encoding', spaces, 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { location }
-    },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
-        lualine_y = {},
-        lualine_z = {}
-    },
-    tabline = {},
-    extensions = {}
-}
+require("lualine").setup({
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = { "alpha", "dashboard", "toggleterm" },
+		always_divide_middle = true,
+		globalstatus = true,
+	},
+	sections = {
+		lualine_a = { "branch" },
+		lualine_b = { { "diff", source = diff_source }, "diagnostics" },
+		lualine_c = { "filename", "lsp_progress", { gps.get_location, cond = gps.is_available } },
+		lualine_x = { "encoding", "filetype" },
+		lualine_y = { "progress" },
+		lualine_z = { location },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	extensions = {},
+})
