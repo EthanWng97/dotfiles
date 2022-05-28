5d8725404f6ff2f9a2e89270b3ed801a7e7c4008
Yifan Wang <yif.wang@icloud.com>
2022-05-28 12:13:20 -0700

    chore: nvim-tree config update

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,6 +1,6 @@
-vim.g.nvim_tree_respect_buf_cwd = 1
 require 'nvim-tree'.setup {
     update_cwd = true,
+    respect_buf_cwd = true,
     update_focused_file = {
         enable = true,
         update_cwd = true,
