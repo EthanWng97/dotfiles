90a4b8336e85c33d5b1d3370a40f9a10efb1b9c8
Yifan Wang <yif.wang@icloud.com>
2022-06-02 12:13:59 -0700

    feat: indent options for nvim-tree

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,14 +1,18 @@
-require 'nvim-tree'.setup {
-    update_cwd = true,
-    respect_buf_cwd = true,
-    update_focused_file = {
-        enable = true,
-        update_cwd = true,
-    },
-    git = {
-        enable = true,
-        ignore = false,
-        timeout = 400,
-    },
-
-}
+require("nvim-tree").setup({
+	update_cwd = true,
+	respect_buf_cwd = true,
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+	},
+	git = {
+		enable = true,
+		ignore = false,
+		timeout = 400,
+	},
+	renderer = {
+		indent_markers = {
+			enable = true,
+		},
+	},
+})
