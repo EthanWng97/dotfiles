afa97dac71a81a6e3a81ab44d6ff3a52a7d82095
Yifan Wang <yif.wang@icloud.com>
2022-06-08 13:53:20 -0700

    chore(nvim): keymap

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -63,3 +63,5 @@ keymap("n", "<leader>ww", ":set wrap!<CR>", opts)
 
 keymap("n", "<leader>nf", ":lua require('neogen').generate()<CR>", opts)
 keymap("n", "<leader>fm", ":lua vim.lsp.buf.format({async = false})<CR>", opts)
+
+keymap("n", "<C-w>", ":bd<cr>", opts)
