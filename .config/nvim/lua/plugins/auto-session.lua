aa471c715b2906d18d3bf8457a2bb9fe9ce96a8f
Yifan Wang <yif.wang@icloud.com>
2022-07-10 19:01:15 -0700

    feat(nvim): auto-session

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -1,10 +1,22 @@
-require('auto-session').setup {
-    log_level = 'info',
-    auto_session_enabled = true,
-    auto_session_create_enabled = false,
-    auto_save_enabled = true,
-    auto_restore_enabled = true
-}
+local status_ok, auto_session = pcall(require, "auto-session")
+if not status_ok then
+	return
+end
 
-vim.o.sessionoptions =
-    "buffers,curdir,folds,help,tabpages,winsize,resize,winpos,terminal"
+auto_session.setup({
+	auto_session_enabled = true,
+	auto_session_create_enabled = false,
+	-- the configs below are lua only
+	bypass_session_save_file_types = { "alpha" },
+})
+
+vim.o.sessionoptions = "buffers,curdir,folds,help,tabpages,winsize,resize,winpos,terminal"
+
+local status_ok, session_lens = pcall(require, "session-lens")
+if not status_ok then
+	return
+end
+
+session_lens.setup({
+	-- path_display = { "shorten" },
+})
