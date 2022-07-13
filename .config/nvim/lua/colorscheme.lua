692f71ab3442dfd9af1f2afa356fb3af7e68faa5
Yifan Wang <yif.wang@icloud.com>
2022-07-12 22:26:22 -0700

    feat(nvim): colorscheme config update

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -4,14 +4,22 @@ vim.cmd([[
     autocmd colorscheme * highlight GitSignsAdd gui=none guifg=#A1C281 guibg=None
     autocmd colorscheme * highlight GitSignsChange gui=none guifg=#74ADEA guibg=None
     autocmd colorscheme * highlight GitSignsDelete gui=none guifg=#FE747A guibg=None
-
-    let g:vscode_italic_comment = 1
-
 ]])
 
-local catppuccin = require("catppuccin")
+local status_ok, vscode = pcall(require, "vscode")
+if not status_ok then
+	return
+end
 
--- configure it
+vscode.setup({
+	italic_comments = true,
+})
+
+local status_ok, catppuccin = pcall(require, "catppuccin")
+if not status_ok then
+	return
+end
+
 catppuccin.setup({
 	transparent_background = false,
 	integrations = {
@@ -35,7 +43,7 @@ onedark.setup({
 	style = "darker",
 })
 
-local colorscheme = "vscode"
+local colorscheme = "catppuccin"
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
 	vim.notify("colorscheme " .. colorscheme .. " not found!")
