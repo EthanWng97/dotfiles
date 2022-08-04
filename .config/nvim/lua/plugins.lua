8cf6491818b43a396522675c1868280b2e1f0412
Yifan Wang <yif.wang@icloud.com>
2022-08-04 14:57:07 -0700

    feat: add wakatime support

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -143,6 +143,7 @@ return packer.startup(function(use)
 	use("Shatur/neovim-session-manager")
 	use("ahmedkhalf/project.nvim")
 	use("aserowy/tmux.nvim")
+    use("wakatime/vim-wakatime")
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
