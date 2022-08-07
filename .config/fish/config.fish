befa9ba1df870ae827cdc8d3e5e56c61f8ce44c5
Yifan Wang <yif.wang@icloud.com>
2022-08-07 15:00:11 -0700

    chore(fish): clean config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,33 +2,28 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# general setting
+fish_add_path ~/.cargo/bin
+fish_add_path /opt/homebrew/bin
+bind ` accept-autosuggestion
+bind -M insert ` accept-autosuggestion
+fzf_configure_bindings --git_status=\cg --git_log=\cl --history=\cr --variables=\cv --directory=\cf --processes=\cp
+
+
+# alias and abbr setting
 alias ls exa
 alias ll "exa -l -g --icons"
 alias la "ll -a"
 alias tn "tmux new -s"
-abbr ta "tmux attach -t"
 alias tl 'tmux list-sessions'
-
-set fish_prompt_pwd_dir_length 0
-set BAT_THEME Dracula
-set -x LS_COLORS (vivid generate dracula)
-set -x STARSHIP_CONFIG "$HOME/.config/starship/config.toml"
-set -x HDF5_DIR /opt/homebrew/Cellar/hdf5/1.13.0
-set -x LG_CONFIG_FILE "/Users/navepnow/.config/lazygit/config.yml"
 alias bat="bat --theme \"Visual Studio Dark+\""
-set fish_greeting
-zoxide init fish | source
-thefuck --alias | source
-
 alias nv nvim
 alias lg lazygit
-
-fish_add_path ~/.cargo/bin
-fish_add_path /opt/homebrew/bin
-
 alias update '~/shell-scripts/./update.sh'
 alias backup '~/shell-scripts/./backup.sh'
 alias install '~/shell-scripts/./install.sh'
+
+abbr ta "tmux attach -t"
 
 function rga-fzf
     set -x RG_PREFIX rga --files-with-matches
@@ -43,14 +38,22 @@ function rga-fzf
         open "$file"
 end
 
-bind ` accept-autosuggestion
-bind -M insert ` accept-autosuggestion
+# command setting
+zoxide init fish | source
+thefuck --alias | source
+starship init fish --print-full-init | source
+
 set fish_cursor_insert line
 set fish_cursor_replace_one underscore
 # fzf_configure_bindings --dir=\cf
-fzf_configure_bindings --git_status=\cg --git_log=\cl --history=\cr --variables=\cv --directory=\cf --processes=\cp
 set fzf_preview_dir_cmd exa --all --color=always --icons
 set fzf_fd_opts --hidden --no-ignore
-starship init fish --print-full-init | source
 set -gx EDITOR nvim
 set -Ux FZF_DEFAULT_OPTS --cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*" --color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4
+set fish_prompt_pwd_dir_length 0
+set BAT_THEME Dracula
+set -x LS_COLORS (vivid generate dracula)
+set -x STARSHIP_CONFIG "$HOME/.config/starship/config.toml"
+set -x HDF5_DIR /opt/homebrew/Cellar/hdf5/1.13.0
+set -x LG_CONFIG_FILE "/Users/navepnow/.config/lazygit/config.yml"
+set fish_greeting
--- .config/fish/functions/_sponge_on_prompt.fish
+++
@@ -1,3 +0,0 @@
-function _sponge_on_prompt --on-event fish_prompt
-  _sponge_remove_from_history
-end
