9fa25515d13422e6802f6792aae639fa66c715e1
Yifan Wang <yif.wang@icloud.com>
2022-09-03 10:42:53 -0700

    chore(nvim): colorizer config update

--- .config/nvim/lua/user/colorizer.lua
+++ .config/nvim/lua/user/colorizer.lua
@@ -1,14 +1,17 @@
-require'colorizer'.setup({
-    '*',
-    css = {
-        RGB = true, -- #RGB hex codes
-        RRGGBB = true, -- #RRGGBB hex codes
-        names = true, -- "Name" codes like Blue
-        RRGGBBAA = true, -- #RRGGBBAA hex codes
-        rgb_fn = true, -- CSS rgb() and rgba() functions
-        hsl_fn = true, -- CSS hsl() and hsla() functions
-        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        css_fn = true -- Enable all CSS *functions*: rgb_fn, hsl_fn
-    },
-    html = {}
+require("colorizer").setup({
+	filetypes = { "*" },
+	user_default_options = {
+		RGB = true, -- #RGB hex codes
+		RRGGBB = true, -- #RRGGBB hex codes
+		names = true, -- "Name" codes like Blue or blue
+		RRGGBBAA = true, -- #RRGGBBAA hex codes
+		AARRGGBB = false, -- 0xAARRGGBB hex codes
+		rgb_fn = true, -- CSS rgb() and rgba() functions
+		hsl_fn = true, -- CSS hsl() and hsla() functions
+		css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+		css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+		-- Available modes for `mode`: foreground, background,  virtualtext
+		mode = "background", -- Set the display mode.
+		virtualtext = "■",
+	},
 })
