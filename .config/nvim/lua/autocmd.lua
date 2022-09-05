620209000261a7feb176ab634a45bbe5ff52d404
Yifan Wang <yif.wang@icloud.com>
2022-09-05 00:21:13 -0700

    chore(nvim): cleanup

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,34 +1,10 @@
--- vim.api.nvim_create_autocmd(
---     { 'CursorHold' }, {
---     pattern = { '*' },
---     callback = function()
---         vim.diagnostic.open_float(nil, { focus = false, scope = "cursor" })
---     end
--- })
--- vim.api.nvim_create_autocmd(
---     { 'BufWritePre' }, {
---     pattern = { '*' },
---     callback = function()
---         vim.lsp.buf.format({ async = false })
---     end
--- })
--- vim.cmd([[
---     autocmd! BufWritePre * lua vim.lsp.buf.format({ async = true })
--- ]])
-
--- Disable folding on alpha buffer
-vim.cmd([[
-    autocmd filetype alpha setlocal nofoldenable
-]])
-
-
 -- Create an autocmd User PackerCompileDone to update it every time packer is compiled
 vim.api.nvim_create_autocmd("User", {
 	pattern = "PackerCompileDone",
 	callback = function()
-		vim.cmd "CatppuccinCompile"
+		vim.cmd("CatppuccinCompile")
 		vim.defer_fn(function()
-			vim.cmd "colorscheme catppuccin"
+			vim.cmd("colorscheme catppuccin")
 		end, 50) -- Debounced for live reloading
 	end,
 })
@@ -37,6 +13,6 @@ vim.api.nvim_create_autocmd("User", {
 vim.api.nvim_create_autocmd("BufWritePost", {
 	pattern = { "plugins.lua", "colorscheme.lua" },
 	callback = function()
-		vim.cmd "PackerCompile"
+		vim.cmd("PackerCompile")
 	end,
 })
--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -23,17 +23,4 @@ null_ls.setup({
 		--           disabled_filetypes = {"NvimTree"}
 		--       }),
 	},
-	-- you can reuse a shared lspconfig on_attach callback here
-	-- on_attach = function(client, bufnr)
-	-- 	if client.supports_method("textDocument/formatting") then
-	-- 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-	-- 		vim.api.nvim_create_autocmd("BufWritePre", {
-	-- 			group = augroup,
-	-- 			buffer = bufnr,
-	-- 			callback = function()
-	-- 				vim.lsp.buf.format({ async = true })
-	-- 			end,
-	-- 		})
-	-- 	end
-	-- end,
 })
