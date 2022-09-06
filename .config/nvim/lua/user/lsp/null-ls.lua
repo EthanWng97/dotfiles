a96b6d1422f302df5feb8a7f702174362729552f
Yifan Wang <yif.wang@icloud.com>
2022-09-06 10:41:13 -0700

    feat(nvim): null-ls

--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -5,6 +5,7 @@ end
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
+local completion = null_ls.builtins.completion
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
@@ -17,7 +18,9 @@ null_ls.setup({
 		formatting.clang_format,
 		formatting.taplo,
 		formatting.black,
+        formatting.fish_indent,
 		diagnostics.codespell,
+        diagnostics.fish,
 		code_actions.gitsigns,
 		-- diagnostics.cspell.with({
 		--           disabled_filetypes = {"NvimTree"}
