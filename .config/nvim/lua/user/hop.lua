0bfff3c3a59c55594c3e1557179edae210d221bd
Yifan Wang <yif.wang@icloud.com>
2022-09-13 10:51:19 -0700

    chore(nvim): hop keymap update

--- .config/nvim/lua/user/hop.lua
+++ .config/nvim/lua/user/hop.lua
@@ -6,4 +6,4 @@ end
 hop.setup({})
 
 -- place this in one of your configuration file(s)
-vim.api.nvim_set_keymap("", "s", "<cmd>lua require'hop'.hint_char1()<cr>", {})
+vim.api.nvim_set_keymap("", "s", "<cmd>lua require'hop'.hint_words()<cr>", {})
