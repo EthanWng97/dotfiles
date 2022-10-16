a627cdaf961999eaf7f54f3cb9bf31add6fbf4b2
Yifan Wang <yif.wang@icloud.com>
2022-10-16 16:01:03 -0700

    fix: typo

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,47 +1,47 @@
 # update brew
 echo "--------------------------------"
-echo "- Updaing Brew Packages        -"
+echo "- Updating Brew Packages        -"
 echo "--------------------------------"
 brew update && brew upgrade && brew cu -a -y --cleanup && brew cleanup && brew autoremove
 
 # update npm packages
 echo "--------------------------------"
-echo "- Updaing NPM Packages         -"
+echo "- Updating NPM Packages         -"
 echo "--------------------------------"
 npm update --location=global
 
 # update python packages
 echo "--------------------------------"
-echo "- Updaing Pip Packages         -"
+echo "- Updating Pip Packages         -"
 echo "--------------------------------"
 pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U
 
-# update python packages
+# update Rust packages
 echo "--------------------------------"
-echo "- Updaing Rust Packages         -"
+echo "- Updating Rust Packages         -"
 echo "--------------------------------"
 cargo install $(cargo install --list | egrep '^[a-z0-9_-]+ v[0-9.]+:$' | cut -f1 -d' ')
 
 # update fish packages
 echo "--------------------------------"
-echo "- Updaing Fish Packages        -"
+echo "- Updating Fish Packages        -"
 echo "--------------------------------"
 fish -c "fisher update"
 
 # update mac apps
 echo "--------------------------------"
-echo "- Updaing Mac Apps             -"
+echo "- Updating Mac Apps             -"
 echo "--------------------------------"
 mas upgrade
 
 # update tmux plugins
 echo "--------------------------------"
-echo "- Updaing Tmux Plugins         -"
+echo "- Updating Tmux Plugins         -"
 echo "--------------------------------"
 ~/.config/tmux/plugins/tpm/bin/update_plugins all
 
 # update nvim plugins
 echo "--------------------------------"
-echo "- Updaing Neovim Plugins       -"
+echo "- Updating Neovim Plugins       -"
 echo "--------------------------------"
 nvim -c 'UpdateAll'
