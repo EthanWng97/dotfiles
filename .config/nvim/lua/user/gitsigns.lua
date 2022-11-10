c417fe494dd2f957ea12245ed76e96a4d09d8861
Yifan Wang <yif.wang@icloud.com>
2022-11-09 17:20:43 -0800

    feat(nvim): gitsigns config updatge

--- .config/nvim/lua/user/gitsigns.lua
+++ .config/nvim/lua/user/gitsigns.lua
@@ -4,6 +4,14 @@ if not status_ok then
 end
 
 gitsigns.setup({
+	signs = {
+		add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+		change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+		delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+		topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+		changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+		untracked = { hl = "GitSignsAdd", text = "┇", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+	},
 	current_line_blame = true,
 	current_line_blame_opts = {
 		virt_text = true,
@@ -12,38 +20,6 @@ gitsigns.setup({
 		ignore_whitespace = false,
 	},
 	current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-	signs = {
-		add = {
-			hl = "GitSignsAdd",
-			text = "▎",
-			numhl = "GitSignsAddNr",
-			linehl = "GitSignsAddLn",
-		},
-		change = {
-			hl = "GitSignsChange",
-			text = "▎",
-			numhl = "GitSignsChangeNr",
-			linehl = "GitSignsChangeLn",
-		},
-		delete = {
-			hl = "GitSignsDelete",
-			text = "契",
-			numhl = "GitSignsDeleteNr",
-			linehl = "GitSignsDeleteLn",
-		},
-		topdelete = {
-			hl = "GitSignsDelete",
-			text = "契",
-			numhl = "GitSignsDeleteNr",
-			linehl = "GitSignsDeleteLn",
-		},
-		changedelete = {
-			hl = "GitSignsChange",
-			text = "▎",
-			numhl = "GitSignsChangeNr",
-			linehl = "GitSignsChangeLn",
-		},
-	},
 	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
 	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
 	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
