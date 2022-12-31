cac9505e4639f2ab7143adfd170161d23e3617f0
Yifan Wang <yif.wang@icloud.com>
2022-12-30 22:49:57 -0800

    chore(nvim): format

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,21 +1,29 @@
 -- All plugins are lazy-loaded by default
 return {
-    { "nvim-lua/plenary.nvim" },
-    -- debug plugins
-    { "puremourning/vimspector" },
-    -- { "mfussenegger/nvim-dap" },
-    -- { "rcarriga/nvim-dap-ui" },
+	{ "nvim-lua/plenary.nvim" },
+	-- debug plugins
+	{ "puremourning/vimspector" },
+	-- { "mfussenegger/nvim-dap" },
+	-- { "rcarriga/nvim-dap-ui" },
 
-    -- UI
-    { "MunifTanjim/nui.nvim" },
-    { "kyazdani42/nvim-web-devicons" },
-    { "folke/tokyonight.nvim" },
-    { "lunarvim/darkplus.nvim", lazy = false },
+	-- UI
+	{ "MunifTanjim/nui.nvim" },
+	{ "kyazdani42/nvim-web-devicons" },
+	{ "folke/tokyonight.nvim" },
+	{ "lunarvim/darkplus.nvim", lazy = false },
 
-    -- Utilities
-    {
-        "folke/persistence.nvim",
-        event = "BufReadPre",
-        config = { options = { "buffers,curdir,folds,help,tabpages,terminal,globals" } },
-    },
+	-- Utilities
+	{
+		"folke/persistence.nvim",
+		event = "BufReadPre",
+		keys = {
+			{
+				"ls",
+				function()
+					require("persistence").load()
+				end,
+			},
+		},
+		config = { options = { "buffers,curdir,folds,help,tabpages,terminal,globals" } },
+	},
 }
