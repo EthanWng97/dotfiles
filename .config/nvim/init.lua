3c078f35b81e5bf40c323c71e03c69199b243aac
Yifan Wang <yif.wang@icloud.com>
2022-12-30 19:52:04 -0800

    feat: lazy config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,10 @@
--- require('plugins')
--- require('commands')
-require('options')
--- require('mappings')
--- require('colorscheme')
-require('lazy-config')
+require("options")
+require("lazy-config")
 
 vim.api.nvim_create_autocmd("User", {
-  pattern = "VeryLazy",
-  callback = function()
-    require("commands")
-    require("mappings")
-  end,
+	pattern = "VeryLazy",
+	callback = function()
+		require("commands")
+		require("mappings")
+	end,
 })
