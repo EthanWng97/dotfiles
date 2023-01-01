36cc0e17609ba66857c33a052f85476f8b4c92fc
Yifan Wang <yif.wang@icloud.com>
2022-12-31 21:25:29 -0800

    feat(nvim): lazy config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,10 @@
-require("options")
-require("lazy-config")
+require("config.options")
+require("config.lazy")
 
 vim.api.nvim_create_autocmd("User", {
     pattern = "VeryLazy",
     callback = function()
-        require("commands")
-        require("mappings")
+        require("config.autocmds")
+        require("config.keymaps")
     end,
 })
