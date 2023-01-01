1530c60b8f700fd3a5c691cd9d1422f9a2e44efd
Yifan Wang <yif.wang@icloud.com>
2022-12-31 21:59:15 -0800

    chore(nvim): update

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,25 +1,25 @@
 local M = {
-	"folke/noice.nvim",
-	event = "VeryLazy",
+    "folke/noice.nvim",
+    event = "VeryLazy",
 }
 
 function M.config()
-	require("noice").setup({
-		lsp = {
-			override = {
-				-- override the default lsp markdown formatter with Noice
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				-- override the lsp markdown formatter with Noice
-				["vim.lsp.util.stylize_markdown"] = true,
-				-- override cmp documentation with Noice (needs the other options to work)
-				["cmp.entry.get_documentation"] = true,
-			},
-		},
-		presets = {
-			command_palette = true,
-			lsp_doc_border = true,
-		},
-	})
+    require("noice").setup({
+        lsp = {
+            override = {
+                -- override the default lsp markdown formatter with Noice
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                -- override the lsp markdown formatter with Noice
+                ["vim.lsp.util.stylize_markdown"] = true,
+                -- override cmp documentation with Noice (needs the other options to work)
+                ["cmp.entry.get_documentation"] = true,
+            },
+        },
+        presets = {
+            command_palette = true,
+            lsp_doc_border = true,
+        },
+    })
 end
 
 return M
