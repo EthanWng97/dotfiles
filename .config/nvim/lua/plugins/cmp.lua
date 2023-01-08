f72b4c4c1751514b004c9e0d07e21b5c48609cf4
Yifan Wang <yif.wang@icloud.com>
2023-01-08 11:17:00 -0800

    chore(nvim): cmp update

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -29,7 +29,7 @@ function M.config()
 		if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
 			return false
 		end
-		local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+		local line, col = vim.F.unpack_len(vim.api.nvim_win_get_cursor(0))
 		return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 	end
 	-- load snippets
