89a077e529a53787aea9d29560f6e943cba216c9
Yifan Wang <yif.wang@icloud.com>
2023-01-14 13:40:20 -0800

    feat(nvim): autocmds

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,19 +1,18 @@
--- Create an autocmd User PackerCompileDone to update it every time packer is compiled
--- vim.api.nvim_create_autocmd("User", {
--- 	pattern = "PackerCompileDone",
--- 	callback = function()
--- 		vim.cmd("CatppuccinCompile")
--- 		vim.defer_fn(function()
--- 			vim.cmd("colorscheme catppuccin")
--- 		end, 50) -- Debounced for live reloading
--- 	end,
--- })
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
 
--- PackerCompile on save assuming your plugin specification file is in plugins.lua or catppuccin.lua
--- vim.api.nvim_create_autocmd("BufWritePost", {
--- 	pattern = { "plugins.lua", "colorscheme.lua" },
--- 	callback = function()
--- 		vim.cmd("PackerCompile")
--- 	end,
--- })
---
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "gitcommit", "markdown" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
+})
