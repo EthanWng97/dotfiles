e5a4c87eb65d1a76f79766e32f4c42bdfa76423b
Yifan Wang <yif.wang@icloud.com>
2023-01-16 23:58:53 -0800

    chore: update

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,16 +1,8 @@
 -- All plugins are lazy-loaded by default
 return {
     { "nvim-lua/plenary.nvim" },
-    -- debug plugins
-    { "puremourning/vimspector" },
-    -- { "mfussenegger/nvim-dap" },
-    -- { "rcarriga/nvim-dap-ui" },
-
-    -- UI
     { "MunifTanjim/nui.nvim" },
     { "nvim-tree/nvim-web-devicons", opts = { default = true } },
-    { "folke/tokyonight.nvim" },
-    { "lunarvim/darkplus.nvim", lazy = false },
 
     -- Utilities
     {
