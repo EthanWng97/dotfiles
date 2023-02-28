c056b684c8c1857ae3173ba45dbadb676019fd44
Yifan Wang <yif.wang@icloud.com>
2023-02-28 10:58:11 -0800

    chore(nvim): statuscolumn config update

--- .config/nvim/lua/plugins/statuscol.lua
+++ .config/nvim/lua/plugins/statuscol.lua
@@ -1,10 +1,25 @@
 local M = {
 	"luukvbaal/statuscol.nvim",
 	event = "VeryLazy",
-	opts = {
-		setopt = true,
-		order = "NSFs",
-		foldfunc = "builtin",
-	},
+	opts = function()
+		local builtin = require("statuscol.builtin")
+		return {
+			setopt = true,
+			-- order = "NSFs",
+			-- Number Sign Fold sep
+			segments = {
+				{
+					text = { builtin.lnumfunc },
+					click = "v:lua.ScLa",
+				},
+				{ text = { "%s" }, click = "v:lua.ScSa" },
+				{
+					text = { builtin.foldfunc, " " },
+					condition = { true, builtin.not_empty },
+					click = "v:lua.ScFa",
+				},
+			},
+		}
+	end,
 }
 return M
