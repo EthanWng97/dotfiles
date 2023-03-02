35996b41f9bd06f8afc787c7ffcd127d4864f3bb
Yifan Wang <yif.wang@icloud.com>
2023-03-02 00:41:18 -0800

    chore(nvim): leap keymap update

--- .config/nvim/lua/plugins/leap.lua
+++ .config/nvim/lua/plugins/leap.lua
@@ -3,7 +3,12 @@ local M = {
     event = "VeryLazy",
     config = function(_, opts)
         local leap = require("leap")
-        leap.add_default_mappings()
+        vim.keymap.set("n", "s", function()
+            local focusable_windows_on_tabpage = vim.tbl_filter(function(win)
+                return vim.api.nvim_win_get_config(win).focusable
+            end, vim.api.nvim_tabpage_list_wins(0))
+            leap.leap({ target_windows = focusable_windows_on_tabpage })
+        end)
     end,
 }
 
