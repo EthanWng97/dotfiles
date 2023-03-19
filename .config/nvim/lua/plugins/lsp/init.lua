08167e6c04795efcd50706174f1b0c7adff519cd
Yifan Wang <yif.wang@icloud.com>
2023-03-19 10:59:10 -0700

    chore: update

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -1,51 +1,60 @@
 local M = {
-    "neovim/nvim-lspconfig",
-    event = "BufReadPre",
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "williamboman/mason-lspconfig.nvim",
-    },
-    config = function(_, _)
-        local utils = require("utils")
-        local mason_lspconfig = require("mason-lspconfig")
-        local lspconfig = require("lspconfig")
-        local lsp_utils = require("plugins.lsp.lsp-utils")
-        lsp_utils.setup()
+	"neovim/nvim-lspconfig",
+	event = "BufReadPre",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function(_, _)
+		local utils = require("utils")
+		local mason_lspconfig = require("mason-lspconfig")
+		local lspconfig = require("lspconfig")
+		local lsp_utils = require("plugins.lsp.lsp-utils")
+		lsp_utils.setup()
 
-        mason_lspconfig.setup({
-            ensure_installed = utils.lsp_servers,
-        })
+		mason_lspconfig.setup({
+			ensure_installed = utils.lsp_servers,
+		})
 
-        mason_lspconfig.setup_handlers({
-            function(server_name)
-                lspconfig[server_name].setup({
-                    on_attach = lsp_utils.on_attach,
-                    capabilities = lsp_utils.capabilities,
-                })
-            end,
-            ["pyright"] = function()
-                lspconfig.pyright.setup({
-                    on_attach = lsp_utils.on_attach,
-                    capabilities = lsp_utils.capabilities,
-                    settings = {
-                        python = {
-                            analysis = {
-                                typeCheckingMode = "off",
-                            },
-                        },
-                    },
-                })
-            end,
-            ["clangd"] = function()
-                local capabilities_cpp = lsp_utils.capabilities
-                capabilities_cpp.offsetEncoding = { "uts-16" }
-                lspconfig.clangd.setup({
-                    on_attach = lsp_utils.on_attach,
-                    capabilities = capabilities_cpp,
-                })
-            end,
-        })
-    end,
+		-- mason_lspconfig.setup_handlers({
+		--     function(server_name)
+		--         lspconfig[server_name].setup({
+		--             on_attach = lsp_utils.on_attach,
+		--             capabilities = lsp_utils.capabilities,
+		--         })
+		--     end,
+		--     ["pyright"] = function()
+		--         lspconfig.pyright.setup({
+		--             on_attach = lsp_utils.on_attach,
+		--             capabilities = lsp_utils.capabilities,
+		--             settings = {
+		--                 python = {
+		--                     analysis = {
+		--                         typeCheckingMode = "off",
+		--                     },
+		--                 },
+		--             },
+		--         })
+		--     end,
+		--     ["clangd"] = function()
+		--         local capabilities_cpp = lsp_utils.capabilities
+		--         capabilities_cpp.offsetEncoding = { "uts-16" }
+		--         lspconfig.clangd.setup({
+		--             on_attach = lsp_utils.on_attach,
+		--             capabilities = capabilities_cpp,
+		--         })
+		--     end,
+		-- })
+		lspconfig.pyright.setup({
+			settings = {
+				python = {
+					analysis = {
+						typeCheckingMode = "off",
+					},
+				},
+			},
+		})
+	end,
 }
 
 return M
